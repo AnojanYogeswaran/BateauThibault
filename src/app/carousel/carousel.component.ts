ba3b367f052fcfae32ad59b8b9ef033cc1317690
Yaël DONAT <donatyael@gmail.com>
2022-11-22 23:48:10 +0100

    update: carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,9 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
-SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
-
-
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
