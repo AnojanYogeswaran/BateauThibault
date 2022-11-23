a48b468b9f5285b39ce2306e79e986ee1a1396d6
Abder100 <ayourmeche@gmail.com>
2022-11-23 17:31:36 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
     redirectTo: 'home',
     pathMatch: 'full'
   },  {
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'boat',
     loadChildren: () => import('./boat/boat.module').then( m => m.BoatPageModule)
   },
@@ -33,9 +47,23 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
     path: 'single-restaurant',
     loadChildren: () => import('./single-restaurant/single-restaurant.module').then( m => m.SingleRestaurantPageModule)
   },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+
+  },
 
 ];
 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
