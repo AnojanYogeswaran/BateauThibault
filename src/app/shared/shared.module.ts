12ff070b03ac23983a9cbd269065fa6eaa1a773e
YaelDonat <donatyael@gmail.com>
2022-11-24 15:59:57 +0100

    add: basket component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ItemComponent } from '../item/item.component';
 import { IonicModule } from '@ionic/angular';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { BasketComponent } from '../basket/basket.component';
 
 
 
 @NgModule({
-  declarations: [ItemComponent, CarouselComponent],
+  declarations: [ItemComponent, CarouselComponent, BasketComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports:[ItemComponent, CarouselComponent]
+  exports:[ItemComponent, CarouselComponent, BasketComponent]
 })
 export class SharedModule { }
